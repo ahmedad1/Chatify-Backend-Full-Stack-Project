25ebd65998dffe95baea2ca57e5ec1cb444a2fc5
ahmedad1 <ahmedadomaror94@gmail.com>
2024-10-15 15:11:34 +0300

    Modified in the frontend

--- Chatify-Project/Chatify/obj/Release/net8.0/Chatify.AssemblyInfo.cs
+++ Chatify-Project/Chatify/obj/Release/net8.0/Chatify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chatify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050381d94b8612ac2f739b8fcab23e2d6495a714")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ce364a1be83f13489cf98228d1977ede3b7038")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chatify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chatify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Chatify-Project/RepositoryPatternUOW.Core/obj/Debug/net8.0/RepositoryPatternUOW.Core.AssemblyInfo.cs
+++ Chatify-Project/RepositoryPatternUOW.Core/obj/Debug/net8.0/RepositoryPatternUOW.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryPatternUOW.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050381d94b8612ac2f739b8fcab23e2d6495a714")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ce364a1be83f13489cf98228d1977ede3b7038")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryPatternUOW.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryPatternUOW.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
